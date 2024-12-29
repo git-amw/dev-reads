e3ea7a9f1152137508e86957172a0effe2e07b22
amw9 <amar75ws@gmail.com>
2024-12-29 18:05:38 +0530

    app init

---
+++ src/main/java/com/devreads/dev_reads/DevReadsApplication.java
@@ -0,0 +1,13 @@
+package com.devreads.dev_reads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevReadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevReadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devreads/dev_reads/DevReadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devreads.dev_reads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevReadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
